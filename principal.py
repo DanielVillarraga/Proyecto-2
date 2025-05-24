b97037250e0b8b2eae64c9a5543570fc7aab3bfc
DanielVillarraga <danielf.villarragag@uqvirtual.edu.co>
2025-05-24 04:25:11 -0500

    Update principal.py Se añaden codigo para reemplazar los separadores
    Se añaden 7 líneas de código al inicio del archivo (luego de los imports) para cambiar los separadores de ";" a ",", esto se puede cambiar a otro separador reemplazando el tipo de separador que se quiere usar en la variable "replace_str".
    
    Ahora para ejecutar el programa se debe colocar el nombre del csv en la fila 14 dentro del "with open ('nombre del archivo.csv,) as f:f

--- principal.py
+++ principal.py
@@ -5,7 +5,18 @@ from graficador import create_visualizations
 import pandas as pd
 import os
 import time
+import re
 
+### Desde la linea 11 a la 18 se tiene un código que transforma los separadores de un dataset de ";" a ","
+find_str = ";"
+replace_str = ","
+
+with open('precipitaciones_españa.csv') as f:
+    file_text = f.read()
+    replaced_text = re.sub(find_str, replace_str, file_text)
+    with open('texto_con_comas.csv', 'w') as r_file:
+        r_file.write(replaced_text)
+        
 def timing(func):
     """Decorador que mide el tiempo de ejecucion"""
     def wrapper (*args,**kwargs):
@@ -22,7 +33,7 @@ def main():
     os.makedirs('outputs/visualizations', exist_ok=True)
     
     # 1. Cargar datos
-    data = load_data('datos/train3.csv')
+    data = load_data('datos_con_comas.csv')
     if data is None:
         return
     
