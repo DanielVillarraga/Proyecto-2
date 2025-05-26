3878087628d410a8d061b7d25ca5d3635bdba463
DanielVillarraga <danielf.villarragag@uqvirtual.edu.co>
2025-05-26 10:57:52 -0500

    Seleccionar columnas
    Se añadió una función en limpiador_datos.py que permite seleccionar las columnas que se quieren analizar preguntándole al usuario

--- limpiador_datos.py
+++ limpiador_datos.py
@@ -20,3 +20,21 @@ def clean_data(data):
     
     print(f"Datos limpiados. Filas originales: {len(data)}, Filas limpias: {len(cleaned)}")
     return cleaned
+
+
+def selecciona_columns(data, columnas_usuario=None):
+    """
+    Escoje las columnas que se quieran analizar
+
+    Parámetros:
+        columnas_usuario (list): Columnas seleccionadas para análisis.
+    """
+
+    # Si no se especifican columnas, se pregunta
+    if columnas_usuario is None:
+        preview = pd.read_csv(data, nrows=5)
+        print("Columnas disponibles:", list(preview.columns))
+        columnas_input = input("¿Qué columnas deseas analizar? (separadas por comas): ")
+        columnas_usuario = [col.strip() for col in columnas_input.split(',') if col.strip() in preview.columns]
+        columnas_usuario = [preview.columns[0]] + [col for col in columnas_usuario if col != preview.columns[0]]
+        return columnas_usuario
